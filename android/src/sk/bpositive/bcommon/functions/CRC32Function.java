1504027e132c069e88d31a0c11e294780d102676
Ján Horváth <jan.horvath.svk@gmail.com>
2017-07-28 17:34:59 +0200

    -fixed crc32 calculation

--- android/src/sk/bpositive/bcommon/functions/CRC32Function.java
+++ android/src/sk/bpositive/bcommon/functions/CRC32Function.java
@@ -1,22 +1,34 @@
 package sk.bpositive.bcommon.functions;
 
 import com.adobe.fre.*;
+import sk.bpositive.bcommon.BCommonExtension;
 
+import java.nio.ByteBuffer;
 import java.util.zip.CRC32;
 
 public class CRC32Function implements FREFunction {
+
+    private static final int BUFFER_SIZE = 4096;
 
     @Override
     public FREObject call(FREContext freContext, FREObject[] freObjects) {
 
         try {
-
             FREByteArray byteArray = (FREByteArray) freObjects[0];
 
             byteArray.acquire();
 
+            byte buffer[] = new byte[BUFFER_SIZE];
             CRC32 crc32 = new CRC32();
-            crc32.update(byteArray.getBytes().array());
+
+            ByteBuffer byteBuffer = byteArray.getBytes();
+            int remainingBytes = byteBuffer.remaining();
+            while (remainingBytes > 0) {
+                int len = remainingBytes > BUFFER_SIZE ? BUFFER_SIZE : remainingBytes;
+                byteBuffer.get(buffer, 0, len);
+                crc32.update(buffer, 0, len);
+                remainingBytes = byteBuffer.remaining();
+            }
             long crc = crc32.getValue();
 
             byteArray.release();
@@ -28,6 +40,10 @@ public class CRC32Function implements FREFunction {
         } catch (FREWrongThreadException e) {
             e.printStackTrace();
         } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
